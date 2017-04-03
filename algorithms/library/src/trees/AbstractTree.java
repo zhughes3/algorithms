e1389371da4d39c648119ed0ae4f79aa6b7772ac
Zachary Hughes <zachary.hughes@bright-wolf.com>
2017-04-03 18:58:27 -0400

    first thoughts about rb tree structure

---
+++ algorithms/library/src/trees/AbstractTree.java
@@ -0,0 +1,5 @@
+package trees;
+
+public abstract class AbstractTree {
+
+}
---
+++ algorithms/library/src/trees/Driver.java
@@ -0,0 +1,10 @@
+package trees;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
